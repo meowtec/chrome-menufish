3e0dcc095a28716d2716d8b57e88000ee9089dcd
Berton Zhu <sairly@126.com>
2014-12-03 14:50:47 +0800

    v1.1.6 -> 1.1.7

--- options/js/options.js
+++ options/js/options.js
@@ -14,8 +14,8 @@ $(function(){
 
   // 更新
   var version = {
-    v: '1.1.6',
-    build: 7
+    v: '1.1.7',
+    build: 8
   }
   $('#cureent-version').html(version.v)
   var updateNotice = function(data){
