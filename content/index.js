3bf620ca8c958a1fe83912644df6eb487611e3e8
Berton Zhu <sairly@126.com>
2014-12-03 15:25:21 +0800

    version

--- content/index.js
+++ content/index.js
@@ -1,6 +1,6 @@
 var btn = document.getElementById('install-button')
-var build = 8
-var version = '1.1.7'
+var build = 0
+var version = '1.1.8'
 var lastBuild = Number(btn.getAttribute('data-build'))
 var lastVersion = btn.getAttribute('data-version')
 
--- options/js/options.js
+++ options/js/options.js
@@ -14,8 +14,8 @@ $(function(){
 
   // 更新
   var version = {
-    v: '1.1.7',
-    build: 8
+    v: '1.1.8',
+    build: 9
   }
   $('#cureent-version').html(version.v)
   var updateNotice = function(data){
