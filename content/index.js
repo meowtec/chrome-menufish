4296a150b7b3654693c765c6493cdb76329cad25
Berton Zhu <sairly@126.com>
2014-12-03 15:04:07 +0800

    提供官网检测版本号

--- content/index.js
+++ content/index.js
@@ -1,3 +1,13 @@
 var btn = document.getElementById('install-button')
-btn.innerHTML = '已安装'
-btn.disabled = true
+var build = 8
+var version = '1.1.7'
+var lastBuild = Number(btn.getAttribute('data-build'))
+var lastVersion = btn.getAttribute('data-version')
+
+if(build === lastBuild){
+  btn.innerHTML = '已安装'
+  btn.disabled = true
+}else if(build<lastBuild{
+  btn.innerHTML = '升级到最新版：v' + lastVersion
+}
+
